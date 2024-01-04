2ce95ea3324a97b8c49947b12fc62aade3419393
KaterynaZhuvak <129985868+KaterynaZhuvak@users.noreply.github.com>
2024-01-04 18:30:48 -0500

    updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Marvelous-Adventures">
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
